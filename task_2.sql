70bb2d9e662ab8fd88bf45fc77da8699d3188af6
haleluya001 <haleluyatigabe1990@gmail.com>
2025-08-16 19:51:45 +0300

    week 8, project 3

--- task_2.sql
+++ task_2.sql
@@ -17,7 +17,7 @@ CREATE TABLE Books (
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- Create the Customers table
@@ -33,7 +33,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the Order_Details table with foreign keys referencing Orders and Books
@@ -42,6 +42,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
